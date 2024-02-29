56de63625909f29bcca83b2ed8e5374af93dd243
Bhavik Gajdhar <bhavik.gajdhar@1rivet.com>
2024-02-29 12:23:21 +0530

    feat : Added auth0 functionality

--- src/core/components/header/Header.jsx
+++ src/core/components/header/Header.jsx
@@ -1,8 +1,36 @@
-import { ActionIcon, rem } from "@mantine/core";
-import { IconSearch } from "@tabler/icons-react";
-import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import {
+  ActionIcon,
+  Avatar,
+  Box,
+  Burger,
+  Button,
+  Flex,
+  Group,
+  Menu,
+  Text,
+  UnstyledButton,
+  rem,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconLogout, IconSearch } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
 
 function Header() {
+  const { user, loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
+  const [opened, { toggle }] = useDisclosure(false);
+
+  const [name, setUserName] = useState(null);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      const userName = user?.nickname?.split(".");
+      const fullName = userName[0] + " " + userName[1];
+      setUserName(fullName);
+    }
+  }, [isAuthenticated]);
+
   return (
     <header className="header">
       <ActionIcon
@@ -13,6 +41,70 @@ function Header() {
       >
         <IconSearch style={{ width: rem(18), height: rem(18) }} />
       </ActionIcon>
+      <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+      {isAuthenticated ? (
+        <Menu
+          width={260}
+          position="bottom-end"
+          transitionProps={{ transition: "pop-top-right" }}
+          withinPortal
+        >
+          <Menu.Target>
+            <UnstyledButton>
+              <Group gap={7}>
+                <Avatar
+                  src={user.picture}
+                  alt={user.nickname}
+                  radius="sm"
+                  size={40}
+                />
+              </Group>
+            </UnstyledButton>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Item>
+              <Flex>
+                <Avatar
+                  src={user.picture}
+                  alt={user.nickname}
+                  radius="sm"
+                  size={40}
+                />
+                <Box ml="md">
+                  <Text
+                    fw={500}
+                    size="sm"
+                    lh={1}
+                    style={{ textTransform: "capitalize" }}
+                  >
+                    {name}
+                  </Text>
+                  <Text fw={500} size="xs" c="gray" lh={1} mt={6}>
+                    {user.email}
+                  </Text>
+                </Box>
+              </Flex>
+            </Menu.Item>
+            <Menu.Divider />
+            <Menu.Item
+              leftSection={
+                <IconLogout
+                  style={{ width: rem(16), height: rem(16) }}
+                  stroke={1.5}
+                />
+              }
+              onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+              }
+              color="red"
+            >
+              Logout
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      ) : (
+        <Button onClick={() => loginWithRedirect()}>Log In</Button>
+      )}
     </header>
   );
 }
