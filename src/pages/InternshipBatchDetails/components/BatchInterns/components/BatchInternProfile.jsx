a9f2a0b448a859a13eee1253d297017710252506
Bhavik Gajdhar <bhavik.gajdhar@1rivet.com>
2024-03-27 17:25:44 +0530

    feat :added file upload functionality

--- src/pages/InternshipBatchDetails/components/BatchInterns/components/BatchInternProfile.jsx
+++ src/pages/InternshipBatchDetails/components/BatchInterns/components/BatchInternProfile.jsx
@@ -1,14 +1,39 @@
-import { Avatar, Box, Flex, Paper, Text, Tooltip } from "@mantine/core";
+import {
+  Avatar,
+  Box,
+  Flex,
+  ThemeIcon,
+  Paper,
+  ActionIcon,
+  Stack,
+  Text,
+  Tooltip,
+  Group,
+  rem,
+  HoverCard,
+} from "@mantine/core";
 import { Breadcrumb } from "../../../../../shared/common-components/Breadcrumb";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getInternDetailsById } from "../utility/service/BatchIntern.service";
-import { IconCalendar, IconMail, IconPhone } from "@tabler/icons-react";
+import { Dropzone, PDF_MIME_TYPE } from "@mantine/dropzone";
+import {
+  IconUpload,
+  IconX,
+  IconTrash,
+  IconCalendar,
+  IconMail,
+  IconPhone,
+  IconDownload,
+  IconFilePlus,
+  IconFileTypePdf,
+} from "@tabler/icons-react";
 
 export const BatchInternProfile = () => {
   let { id, batchId } = useParams();
   const [batchName, setBatchName] = useState();
   const [internProfile, setInternProfile] = useState();
+  const [files, setFiles] = useState([]);
   const items = [
     { title: "Internship", href: "#" },
     { title: "Intern-Batch", href: "/intern-batch" },
@@ -16,7 +41,8 @@ export const BatchInternProfile = () => {
     {
       title: `${
         internProfile?.firstName.charAt(0).toUpperCase() +
-        internProfile?.firstName.slice(1)+" "+
+        internProfile?.firstName.slice(1) +
+        " " +
         internProfile?.lastName.charAt(0).toUpperCase() +
         internProfile?.lastName.slice(1)
       }`,
@@ -35,8 +61,81 @@ export const BatchInternProfile = () => {
       });
     }
   }, [id]);
+
+  const setFilesList = (file) => {
+    setFiles([...files, ...file]);
+  };
+  const downloadFile = (file, isPreview = false) => {
+    const url = URL.createObjectURL(file);
+    const link = document.createElement("a");
+    link.href = url;
+    if (!isPreview) {
+      link.download = file.name;
+    } else {
+      link.target = file.name;
+    }
+    link.click();
+    URL.revokeObjectURL(url);
+  };
+  const previews = files.map((file, index) => {
+    return (
+      <Box key={index} p={"5px"}>
+        <Group align={"left"} style={{ flexDirection: "column" }}>
+          <Group justify="center">
+            <HoverCard width={90} shadow="sm">
+              <HoverCard.Target>
+                <ThemeIcon
+                  variant="light"
+                  size={60}
+                  onClick={() => downloadFile(file, true)}
+                >
+                  <IconFileTypePdf size={40} stroke={1.5} />
+                </ThemeIcon>
+              </HoverCard.Target>
+              <HoverCard.Dropdown>
+                <ActionIcon
+                  align="center"
+                  justify="center"
+                  ml={"auto"}
+                  onClick={() => {
+                    let removeFile = files.filter(
+                      (data, indexF) => indexF !== index
+                    );
+                    setFiles(removeFile);
+                  }}
+                >
+                  <IconTrash />
+                </ActionIcon>
+                <IconDownload
+                  onClick={() => downloadFile(file, false)}
+                ></IconDownload>
+              </HoverCard.Dropdown>
+            </HoverCard>
+          </Group>
+          <Box>
+            <Tooltip label={file.name + " " + file.size + " KB"}>
+              <Group onClick={() => downloadFile(file, true)}>
+                <Text fw={"bold"}>
+                  {file.name.length < 10
+                    ? file.name
+                    : `${file.name.slice(0, 6)}...${file.name.slice(
+                        file.name.length - 8
+                      )}`}
+                </Text>
+              </Group>
+            </Tooltip>
+          </Box>
+        </Group>
+      </Box>
+    );
+  });
+
   return (
-    <Flex direction="column" className="content-wrapper">
+    <Flex
+      direction="column"
+      className="content-wrapper"
+      style={{ overflowY: "auto" }}
+    >
       <Flex align="center" className="sub-header">
         <div>
           <Breadcrumb data={items} />
@@ -144,10 +243,77 @@ export const BatchInternProfile = () => {
           </Box>
         </Flex>
       </Paper>
-      <Paper withBorder radius="md" m="lg">
-        <Flex style={{ padding: "20px" }}>
-          <h5>Assessment Result</h5>
-        </Flex>
+      <Paper
+        withBorder
+        display={"flex"}
+        radius="md"
+        m="lg"
+        style={{ padding: 20 }}
+      >
+        {previews.length > 0 ? (
+          <Box style={{ overflowX: "auto" }} radius="md">
+            <Flex>{previews}</Flex>
+          </Box>
+        ) : (
+          <></>
+        )}
+        <Dropzone
+          onDrop={(files) => setFilesList(files)}
+          onReject={(files) => console.log("rejected files", files)}
+          maxSize={5 * 1024 ** 2}
+          accept={PDF_MIME_TYPE}
+        >
+          <Stack
+            justify={"center"}
+            align={"left"}
+            style={{ minHeight: 80, pointerEvents: "none" }}
+          >
+            <Dropzone.Accept>
+              <ThemeIcon variant="light" radius={"xl"} size={60}>
+                <IconUpload
+                  style={{
+                    width: rem(52),
+                    height: rem(52),
+                    color: "var(--mantine-color-blue-6)",
+                  }}
+                  stroke={1.5}
+                />
+              </ThemeIcon>
+            </Dropzone.Accept>
+            <Dropzone.Reject>
+              <ThemeIcon
+                variant="light"
+                color={"orange"}
+                radius={"xl"}
+                size={60}
+              >
+                <IconX
+                  style={{
+                    width: rem(52),
+                    height: rem(52),
+                    color: "var(--mantine-color-red-6)",
+                  }}
+                  stroke={1.5}
+                />
+              </ThemeIcon>
+            </Dropzone.Reject>
+            <Dropzone.Idle>
+              <Group
+                align="center"
+                style={{ flexDirection: "column" }}
+                p={"5px"}
+                pl={"20px"}
+              >
+                <ThemeIcon variant="light" size={60}>
+                  <IconFilePlus size={40} stroke={1.5}></IconFilePlus>
+                </ThemeIcon>
+                <Box align="center">
+                  <Text fw={"bold"}>Add New</Text>
+                </Box>
+              </Group>
+            </Dropzone.Idle>
+          </Stack>
+        </Dropzone>
       </Paper>
     </Flex>
   );
